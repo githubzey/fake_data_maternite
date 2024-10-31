1df9e6c1642111a56fd8762e82e5c3651a3cdada
githubzey <zeyneperdemfr@gmail.com>
2024-10-31 21:26:14 +0100

    modifications for more realistic distribution

--- fake_data_generator.py
+++ fake_data_generator.py
@@ -130,7 +130,7 @@ listed_columns_info = ["Groupage_num_CMD", "Num_GHM", "Groupage_code_retour", "N
                        "Contexte_patient_surveillance_particuliere", "Administration_produit_RH", "Rescrit_tarifaire", 
                        "Categorie_nombre_interventions_totales", "Non_Programme_NP", "Passage_structure_urgences"]
                         
-listed_za_columns_info = ["Code_CCAM", "Extension_PMSI", "Phase", "Activite", "Modificateurs", "Remboursement_exceptionnel" ]
+listed_za_columns_info = ["Extension_PMSI", "Phase", "Activite", "Modificateurs", "Remboursement_exceptionnel" ]
 
 # Function to format data according to Cadrage/Remplissage rules
 
@@ -175,14 +175,6 @@ def generate_fake_data(data_type, length, fill_type="Gauche/Espace"):
 
 # Specific data generation functions for the special columns
 
-
-#def generate_sexe(length, fill_type):
-#    return format_data(random.choice(["1", "2", "3"]), length, fill_type)
-
-
-#def generate_mode_entree(length, fill_type):
-#    return format_data(random.choice(["6", "7", "0", "8", "N", "O"]), length, fill_type)
-
     # Generate Provenance based on mode of entry
 def generate_provenance(mode_entree, length, fill_type):
     if mode_entree in ["6", "7", "0"]:
@@ -193,9 +185,6 @@ def generate_provenance(mode_entree, length, fill_type):
         random.choice(["", liste_Provenance_mode_entree]), length, fill_type
     )
 
-
-#def generate_mode_sortie(length, fill_type):
-#    return format_data(random.choice(["6", "7", "0", "8", "9"]), length, fill_type)
 
     # Generate Destination based on mode of exit
 def generate_destination(mode_sortie, length, fill_type):
@@ -215,16 +204,73 @@ def generate_destination(mode_sortie, length, fill_type):
 #     return format_data(str(code_postal), length, fill_type)
 
 # as grammes
-poids_nouveau_ne_min = configs["poids_nouveau_ne_min"]
-poids_nouveau_ne_max = configs["poids_nouveau_ne_max"]
+poids_nouveau_ne_min = int(configs["poids_nouveau_ne_min"])
+poids_nouveau_ne_max = int(configs["poids_nouveau_ne_max"])
 def generate_poids_nouveau_ne(length, fill_type, min_value=poids_nouveau_ne_min, max_value=poids_nouveau_ne_max):
     return format_data(fake.random_int(min=min_value, max=max_value), length, fill_type)
-# as weeks
-age_gestationnel_min = configs["age_gestationnel_min"]
-age_gestationnel_max = configs["age_gestationnel_max"]
-def generate_age_gestationnel(length, fill_type, min_value=age_gestationnel_min, max_value=age_gestationnel_max):
-    return format_data(fake.random_int(min=min_value, max=max_value), length, fill_type)
 
+
+# as weeks
+age_gestationnel_min = int(configs["age_gestationnel_min"])
+age_gestationnel_max = int(configs["age_gestationnel_max"])
+
+
+def generate_age_gestationnel(length, fill_type, min_value=age_gestationnel_min, max_value=age_gestationnel_max):
+    # Decide if the generated age should be full-term or preterm based on probability
+    is_full_term = random.random() < 0.98  # 98% chance for full-term
+    
+    if is_full_term:
+        # Generate a full-term age between 37 and max_value
+        age = fake.random_int(min=37, max=max_value)
+    else:
+        # Generate a preterm age between min_value and 36
+        age = fake.random_int(min=min_value, max=36)
+
+    return format_data(age, length, fill_type)
+
+
+date_naissance_start = datetime.strptime(configs["date_naissance_start"], "%Y-%m-%d")
+date_naissance_end = datetime.strptime(configs["date_naissance_end"], "%Y-%m-%d")
+
+
+
+def generate_date_naissance(length, fill_type, date_naissance_start, date_naissance_end, baby_percentage=0.53):
+    # Decide if the entry should be a baby or a mother based on the given percentage
+    is_baby = random.random() < baby_percentage
+
+    if is_baby:
+        # Generate a birth date for the baby within the specified date range (e.g., within 2023)
+        date_naissance = fake.date_between(start_date=date_naissance_start, end_date=date_naissance_end)
+    else:
+        # Define age groups and weights to prioritize 30-34, followed by 25-29
+        age_groups = [(17, 19), (20, 24), (25, 29), (30, 34), (35, 39), (40, 44), (45, 50)]
+        weights = [0.05, 0.1, 0.25, 0.4, 0.1, 0.07, 0.03]  # Adjusted to prioritize 30-34 and 25-29
+        
+        # Select an age group based on the weights
+        selected_group = random.choices(age_groups, weights=weights, k=1)[0]
+        
+        # Generate a random age within the selected age group
+        mother_age = random.randint(selected_group[0], selected_group[1])
+        
+        # Calculate mother's birth year based on the chosen age
+        mother_birth_year = date_naissance_end.year - mother_age
+        mother_birth_start = datetime(mother_birth_year, 1, 1)
+        mother_birth_end = datetime(mother_birth_year, 12, 31)
+        
+        # Generate a birth date within the calculated range for the mother
+        date_naissance = fake.date_between(start_date=mother_birth_start, end_date=mother_birth_end)
+
+    # Format the date and return it
+    return format_data(date_naissance.strftime("%d%m%Y"), length, fill_type)
+
+
+
+date_ddr_start = datetime.strptime(configs["date_ddr_start"], "%Y-%m-%d")
+date_ddr_end = datetime.strptime(configs["date_ddr_end"], "%Y-%m-%d")
+
+
+def generate_date_derniere_regle(length, fill_type, date_ddr_start, date_ddr_end):
+    return format_data(fake.date_between(start_date=date_ddr_start, end_date=date_ddr_end).strftime("%d%m%Y"), length, fill_type)
 
 # def generate_diagnostic_principal(length, fill_type):
 #     return format_data(random.choice(configs["liste_diagnostic_principal"]), length, fill_type)
@@ -243,6 +289,23 @@ def generate_random_choice_from_liste(length, fill_type, liste):
     return format_data(random.choice(liste), length, fill_type)
 
 
+
+def generate_code_CCAM(length, fill_type, liste):
+    # List of high-priority values with more weight
+    high_priority_values = ['JQGD001', 'JQGD002', 'JQGD003', 'JQGD004', 'JQGD005', 
+                            'JQGD007', 'JQGD008', 'JQGD010', 'JQGD012', 'JQGD013']
+    
+    # Set up weights for each element in the list
+    weights = [8 if item in high_priority_values else 1 for item in liste]  # Give higher weight to high-priority items
+
+    # Use random.choices with weights to select an item
+    selected_item = random.choices(liste, weights=weights, k=1)[0]
+    
+    # Format the selected item
+    return format_data(selected_item, length, fill_type)
+
+
+
 def generate_zones_acte(row):
     za_str = ""
 
@@ -251,6 +314,9 @@ def generate_zones_acte(row):
             start_date = datetime.strptime(row["Date_entree_unite_medicale"], "%d%m%Y")
             end_date = datetime.strptime(row["Date_sortie_unite_medicale"], "%d%m%Y")
             za_str += format_data(fake.date_between(start_date=start_date, end_date=end_date).strftime("%d%m%Y"), length, fill_type)
+
+        elif column_name == "Code_CCAM":
+            za_str += generate_code_CCAM(length, fill_type, liste)   
 
         elif column_name in listed_za_columns_info:
             za_str += generate_random_choice_from_liste(length, fill_type, liste)
@@ -295,9 +361,8 @@ def generate_yearly_fake_data(year, num_records):
 
     min_days_diff = configs["min_days_diff"]  # min differrence days between Date_entree_unite_medicale and Date_sortie_unite_medicale
     max_days_diff = configs["max_days_diff"]  # max differrence days between Date_entree_unite_medicale and Date_sortie_unite_medicale
-    min_age = configs["min_age"] # min age of patient
-    max_age = configs["max_age"] # max age of patient
-
+  
+    
     # Define min and max values for columns
     value_ranges = {
         "Date_entree_unite_medicale": {
@@ -305,7 +370,6 @@ def generate_yearly_fake_data(year, num_records):
             "end_date": datetime(year, 12, 31),
         },
         "Date_sortie_unite_medicale": {"min_days_diff": min_days_diff, "max_days_diff": max_days_diff},
-        "Date_naissance": {"min_age": min_age, "max_age": max_age},
         "Poids_nouveau_ne_entree_unite_medicale": {"min_value": poids_nouveau_ne_min, "max_value": poids_nouveau_ne_max},
         "Age_gestationnel": {"min_value": age_gestationnel_min, "max_value": age_gestationnel_max},
         # Add more ranges as needed
@@ -376,17 +440,18 @@ def generate_yearly_fake_data(year, num_records):
                 )
                 for i in range(num_records)
             ]
-        elif column_name == "Date_naissance":
-            min_age = value_ranges[column_name]["min_age"]
-            max_age = value_ranges[column_name]["max_age"]
 
-            data[column_name] = [
-                format_data(fake.date_of_birth(minimum_age=min_age, maximum_age=max_age).strftime("%d%m%Y"), length, fill_type)
-                for _ in range(num_records)
-            ]
-            # data[column_name] = [
-            #     "X" * 4 + data[column_name][_][4:9] for _ in range(num_records)
-            # ]
+        
+        elif column_name == "Date_naissance":
+            
+            data[column_name] = [generate_date_naissance(length, fill_type, date_naissance_start, date_naissance_end) for _ in range(num_records)]
+            ## Load start and end dates from config and parse as datetime objects
+            #date_naissance_start = value_ranges[column_name]["date_naissance_start"]
+            #date_naissance_end = value_ranges[column_name]["date_naissance_end"]
+            #data[column_name] = [
+            #    format_data(fake.date_between(start_date=date_naissance_start, end_date=date_naissance_end).strftime("%d%m%Y"), length, fill_type)
+            #    for _ in range(num_records)
+            #        ]
         
         elif column_name == "Provenance_mode_entree":
             data[column_name] = [
@@ -441,6 +506,11 @@ def generate_yearly_fake_data(year, num_records):
                 for _ in range(num_records)
             ]
 
+        elif column_name == "Date_dernières_regles":
+             data[column_name] = [
+                 generate_date_derniere_regle(length, fill_type, date_ddr_start, date_ddr_end) for _ in range(num_records)
+             ]
+
         # elif column_name == "Sexe":
         #     data[column_name] = [
         #         generate_sexe(length, fill_type) for _ in range(num_records)
@@ -479,6 +549,31 @@ def generate_yearly_fake_data(year, num_records):
     df = pd.DataFrame(data)
 
     # Constant columns (to define some constant values for some columns)
+    
+    df["Age_gestationnel"] = df["Age_gestationnel"].astype(int)
+        
+    def modify_poids_nouveau_ne(gestational_age):
+        if gestational_age < 31:
+            # Extremely preterm weight range
+            poids = fake.random_int(min=poids_nouveau_ne_min, max=1500)
+        elif 31 <= gestational_age < 37:
+            # Moderate to late preterm weight range
+            poids = fake.random_int(min=1500, max=2500)
+        elif 37 <= gestational_age <= age_gestationnel_max:
+            # Full-term weight range
+            poids = fake.random_int(min=2500, max=poids_nouveau_ne_max)
+        else:
+            # Fallback in case of any unexpected gestational age
+            poids = fake.random_int(min=poids_nouveau_ne_min, max=poids_nouveau_ne_max)
+
+        return poids
+    
+    
+
+
+
+
+    df["Poids_nouveau_ne_entree_unite_medicale"] = df["Age_gestationnel"].apply(modify_poids_nouveau_ne)
     df["Groupage_version_classification"] = configs["Groupage_version_classification"]
     df["Filler"] = configs["Filler"]
     df["Filler_2"] = configs["Filler_2"]
